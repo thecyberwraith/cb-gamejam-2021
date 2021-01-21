58f0d4aa4cc52b7781ab5904f88592412f18d232
Michael Lugo <michaelrlugo@gmail.com>
2021-01-20 20:47:25 -0500

    Added basic menus. Now to add a level.

---
+++ Menus/LevelsList.gd
@@ -0,0 +1,22 @@
+extends GridContainer
+
+onready var back_button: Button = get_node("../BackButton")
+
+const LEVELS_LIST: Array = [
+	[ 'Level 1', 'res://Levels/Level1/Level1.tscn']
+]
+const MainMenu = "res://Menus/MainMenu.tscn"
+
+func _ready():
+	back_button.connect("pressed", get_tree(), "change_scene", [MainMenu])
+	
+	for level_info in LEVELS_LIST:
+		var name = level_info[0]
+		var scene = level_info[1]
+		
+		var button = Button.new()
+		button.text = name
+		button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		button.size_flags_vertical = Control.SIZE_EXPAND_FILL
+		
+		add_child(button)
