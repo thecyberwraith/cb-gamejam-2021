0de16e64b16d885c0959d72238a681fa84cff7d4
Michael Lugo <michaelrlugo@gmail.com>
2021-01-22 15:28:04 -0500

    Placing the bulb wins the game!!!!

--- Items/Bulb/BulbBase.gd
+++ Items/Bulb/BulbBase.gd
@@ -4,7 +4,18 @@ onready var bulb: Sprite = get_node("Bulb")
 onready var base: Sprite = get_node("Base")
 onready var interaction = get_node("InteractableBulbBaseArea")
 
+signal on_bulb_placed
+signal on_bulb_removed
+
 func _ready():
 	bulb.visible = false
-	interaction.connect("on_place_bulb", bulb, "set", ["visible", true])
-	interaction.connect("on_remove_bulb", bulb, "set", ["visible", false])
+	interaction.connect("on_place_bulb", self, "on_bulb_placed_callback")
+	interaction.connect("on_remove_bulb", self, "on_bulb_placed_callback")
+
+func on_bulb_placed_callback():
+	bulb.visible = true
+	emit_signal("on_bulb_placed")
+
+func on_bulb_removed_callback():
+	bulb.visible = false
+	emit_signal("on_bulb_removed")
--- Levels/Level1/Level.gd
+++ Levels/Level1/Level.gd
@@ -4,7 +4,12 @@ onready var starts = get_node("Starts")
 onready var astronaut_handler = get_node("AstronautHandler")
 onready var camera = get_node("AstronautCamera")
 onready var crate = get_node("Item")
+onready var base = get_node("BulbBase")
 
 func _ready():
 	astronaut_handler.initialize(starts)
 	camera.initialize(astronaut_handler)
+	base.connect("on_bulb_placed", self, "level_completed")
+
+func level_completed():
+	get_tree().change_scene("res://Menus/LevelsMenu.tscn")
