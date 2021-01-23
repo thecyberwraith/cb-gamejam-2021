d97577fead1b7c5e371c4b7402fd70eeb7a14baf
Michael Lugo <michaelrlugo@gmail.com>
2021-01-22 22:06:09 -0500

    Added time reporting to mission results.

--- Levels/LevelCore.gd
+++ Levels/LevelCore.gd
@@ -8,10 +8,15 @@ onready var success_menu = get_node("GUI/SuccessMenu")
 onready var camera = get_node("AstronautCamera")
 onready var astronaut_handler = get_node("AstronautHandler")
 
+var elapsed_time
+
 func _ready():
 	astronaut_handler.initialize(starts)
 	camera.initialize(astronaut_handler)
+	elapsed_time = 0
+
+func _process(delta):
+	elapsed_time += delta
 
 func level_success():
-	success_menu.visible = true
-	get_tree().paused = true
+	success_menu.show_results(elapsed_time)
--- Menus/SuccessMenu.gd
+++ Menus/SuccessMenu.gd
@@ -1,13 +1,19 @@
-extends VBoxContainer
+extends Control
 
-onready var time = get_node("Time")
-onready var to_levels = get_node("Buttons/Return")
-onready var quit = get_node("Buttons/Quit")
+onready var time: Label = get_node("PanelContainer/CenterContainer/Time")
+onready var to_levels: Button = get_node("PanelContainer/CenterContainer/Buttons/Return")
+onready var quit: Button = get_node("PanelContainer/CenterContainer/Buttons/Quit")
 
 const LEVEL_SELECT = "res://Menus/LevelsMenu.tscn"
+
 func _ready():
 	to_levels.connect("pressed", self, "return_to_level_select")
 	quit.connect("pressed", get_tree(), "quit")
+
+func show_results(seconds: float):
+	time.text = 'Mission Duration: %s seconds' % [seconds]
+	self.visible = true
+	get_tree().paused = true
 
 func return_to_level_select():
 	get_tree().paused = false
