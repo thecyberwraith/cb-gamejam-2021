97c3ee16662ee46948fd64e30ab8326a7cac55d1
Michael Lugo <michaelrlugo@gmail.com>
2021-01-23 16:32:37 -0500

    Missed a place where I called quit.

--- Menus/SuccessMenu.gd
+++ Menus/SuccessMenu.gd
@@ -4,17 +4,18 @@ onready var time: Label = get_node("PanelContainer/CenterContainer/Time")
 onready var to_levels: Button = get_node("PanelContainer/CenterContainer/Buttons/Return")
 onready var quit: Button = get_node("PanelContainer/CenterContainer/Buttons/Quit")
 
-const LEVEL_SELECT = "res://Menus/LevelsMenu.tscn"
+const LevelSelect = preload("res://Menus/LevelsMenu.tscn")
+const QuitMenu = preload("res://Menus/QuitMenu.tscn")
 
 func _ready():
-	to_levels.connect("pressed", self, "return_to_level_select")
-	quit.connect("pressed", get_tree(), "quit")
+	to_levels.connect("pressed", self, "switch_scene_to", [LevelSelect])
+	quit.connect("pressed", self, "switch_scene_to", [QuitMenu])
 
 func show_results(seconds: float):
 	time.text = 'Mission Duration: %s seconds' % [seconds]
 	self.visible = true
 	get_tree().paused = true
 
-func return_to_level_select():
+func switch_scene_to(scene: PackedScene):
 	get_tree().paused = false
-	get_tree().change_scene(LEVEL_SELECT)
+	get_tree().change_scene_to(scene)
