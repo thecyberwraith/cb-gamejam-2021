fff88a944ef50c96043680283baf0db0467bc184
Michael Lugo <michaelrlugo@gmail.com>
2021-01-24 10:37:48 -0500

    Made level menu prettier.

---
+++ Menus/LevelButton.gd
@@ -0,0 +1,24 @@
+extends Control
+
+onready var button = get_node("VBoxContainer/Button")
+onready var level = get_node("VBoxContainer/Level")
+onready var description = get_node("VBoxContainer/Description")
+onready var time = get_node("VBoxContainer/Time")
+
+signal play_level
+
+var scene: String
+
+func _ready():
+	button.connect("pressed", self, "share_button")
+
+func share_button():
+	emit_signal("play_level", scene)
+
+func populate(a_scene: String, lvl: int, desc: String, best: float):
+	button.disabled = false
+	level.text = "Level %s" % lvl
+	if best > 0:
+		time.text = "Best time: %s seconds" % best
+	description.text = desc
+	scene = a_scene
--- Menus/LevelsList.gd
+++ Menus/LevelsList.gd
@@ -1,5 +1,7 @@
 extends GridContainer
 
+const LevelButton: PackedScene = preload("res://Menus/LevelButton.tscn")
+
 onready var back_button: Button = get_node("../BackButton")
 
 const LEVELS_LIST: Array = [
@@ -8,21 +10,21 @@ const LEVELS_LIST: Array = [
 	[ 'Who drew the short straw?', 'res://Levels/Level3/Level3.tscn'],
 	[ 'Just stay fixed!', 'res://Levels/Level4/Level4.tscn'],
 	[ 'Where is the air going?', 'res://Levels/Level5/Level5.tscn'],
-	[ 'Nothing is Broken!', 'res://Levels/Level6/Level6.tscn']
+	[ 'Nothing is Broken!', 'res://Levels/Level6/Level6.tscn'],
+	[ 'Stupid bulbs...', 'res://Levels/Level7/Level7.tscn']
 ]
 const MainMenu = "res://Menus/MainMenu.tscn"
 
 func _ready():
 	back_button.connect("pressed", get_tree(), "change_scene", [MainMenu])
 	
-	for level_info in LEVELS_LIST:
-		var name = level_info[0]
-		var scene = level_info[1]
+	for i in len(LEVELS_LIST):
+		var name = LEVELS_LIST[i][0]
+		var scene = LEVELS_LIST[i][1]
 		
-		var button = Button.new()
-		button.text = name
-		button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-		button.size_flags_vertical = Control.SIZE_EXPAND_FILL
-		button.connect("pressed", get_tree(), "change_scene", [scene])
-		
+		var button = LevelButton.instance()
 		add_child(button)
+		button.populate(scene, i+1, name, -1)
+		button.connect("play_level", get_tree(), "change_scene", [scene])
+		
+
