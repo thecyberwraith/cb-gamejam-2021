e75af44dc8657deb996d4ab33938b096c98a6c56
Michael Lugo <michaelrlugo@gmail.com>
2021-01-24 15:05:18 -0500

    Updated 2 levels to use todos.

--- Levels/Level1/Level1.gd
+++ Levels/Level1/Level1.gd
@@ -4,4 +4,11 @@ onready var level_core = get_node("LevelCore")
 onready var base = get_node("BulbBase")
 
 func _ready():
-	base.connect("on_bulb_placed", level_core, "level_success")
+	var todos: Todos = level_core.get_todos()
+	todos.add_todo(Todos.Tasks.BULB, 1)
+	todos.connect("all_tasks_complete", level_core, "level_success")
+	base.connect("on_bulb_placed", self, "bulb_placed")
+	
+func bulb_placed():
+	level_core.get_todos().update_todo(Todos.Tasks.BULB, 1)
+
--- Levels/Level2/Level2.gd
+++ Levels/Level2/Level2.gd
@@ -3,16 +3,16 @@ extends Node2D
 onready var level_core = get_node("LevelCore")
 onready var base_1 = get_node("BulbBase")
 onready var base_2 = get_node("BulbBase2")
+onready var todos = level_core.get_todos()
 
-var bases_filled = 0
-
+var bulbs_placed: int = 0
 func _ready():
+	todos.add_todo(Todos.Tasks.BULB, 2)
+	todos.connect("all_tasks_complete", level_core, "level_success")
 	for base in [base_1, base_2]:
 		base.connect("on_bulb_placed", self, "change_bulb_count", [1])
 		base.connect("on_bulb_removed", self, "change_bulb_count", [-1])
 	
 func change_bulb_count(amt: int):
-	bases_filled += amt
-	
-	if bases_filled == 2:
-		level_core.level_success()
+	bulbs_placed += 1
+	todos.update_todo(Todos.Tasks.BULB, bulbs_placed)
