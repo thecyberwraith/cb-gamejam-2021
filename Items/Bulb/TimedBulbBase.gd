c475f1d001acd41797dd10f84eeb2fc9b76297cf
Michael Lugo <michaelrlugo@gmail.com>
2021-01-23 21:07:23 -0500

    Added timed bulb base.

---
+++ Items/Bulb/TimedBulbBase.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+class_name TimedBulbBase
+
+signal net_bulb_change
+
+export var duration: float = 3
+
+onready var base: BulbBase = get_node("BulbBase")
+onready var timer: Timer = get_node("Timer")
+onready var progress: ProgressBar = get_node("ProgressBar")
+
+func _ready():
+	base.connect("on_bulb_placed", self, "start_countdown")
+	timer.connect("timeout", self, "perform_ejection")
+	progress.max_value = duration
+	timer.wait_time = duration
+
+func _process(_delta):
+	progress.value = timer.time_left
+
+func start_countdown():
+	emit_signal("net_bulb_change", 1)
+	progress.visible = true
+	progress.value = duration
+	timer.start(duration)
+
+func perform_ejection():
+	base.eject_bulb()
+	emit_signal("net_bulb_change", -1)
+	progress.visible = false
